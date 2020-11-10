182ee68adb1e831029c1d773579c19de7d5bb4d7
p0isonBR <65384918+p0isonBR@users.noreply.github.com>
2020-11-09 21:26:53 -0300

    Update catcher.py

--- catcher.py
+++ catcher.py
@@ -124,7 +124,8 @@ try:
             with open(lista, 'a+') as socks5:
                 socks5.write(proxys)
                 
-    print(str(len(open(lista).readlines()))+' proxies salvas em '+B+lista+C)
+    print(B+str(len(open(lista).readlines()))+' proxies salvas em '+B+lista+C)
+    print(f'{C}Me acompanhe no Github: {G}https://github.com/p0isonBR{C}')
 
 except(KeyboardInterrupt):
     print(f'\n{C}[{R}!{C}] Interrompido pelo usuãrio!')
